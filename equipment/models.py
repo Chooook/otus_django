54531e31cc7263ec3db53237521bad27cfb203f4
Egor Batarchuk <egr311@ya.ru>
2023-02-22 01:16:47 +0300

    add str methods in equipment models

--- equipment/models.py
+++ equipment/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(unique=True, max_length=32)
 
+    def __str__(self):
+        return self.name
+
 
 class Equipment(models.Model):
 
@@ -12,3 +15,6 @@ class Equipment(models.Model):
 
     name = models.CharField(max_length=64)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
