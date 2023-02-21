15675b523cc18f12f51860db50687c2e7441ffbf
Egor Batarchuk <egr311@ya.ru>
2023-02-22 01:03:05 +0300

    add equipment model initial describe

--- equipment/models.py
+++ equipment/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(unique=True, max_length=32)
+
+
+class Equipment(models.Model):
+
+    class Meta:
+        unique_together = ('name', 'category')
+
+    name = models.CharField(max_length=64)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
