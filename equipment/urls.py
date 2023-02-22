d6c5ec46b90205df7d4b7fdd41b29fc0967017e3
Egor Batarchuk <egr311@ya.ru>
2023-02-22 20:25:08 +0300

    rename equipment_list to equipment rename equipment_list_view to equipment_view add stuff to equipment template: * bootstrap html and js * footer with buttons * header buttons

--- equipment/urls.py
+++ equipment/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import equipment_list_view
+from .views import equipment_view
 
 app_name = 'equipment'
 
 urlpatterns = [
-    path('', equipment_list_view),
+    path('', equipment_view),
 ]
--- equipment/views.py
+++ equipment/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from .models import Equipment
 
 
-def equipment_list_view(request):
+def equipment_view(request):
     equipment_list = Equipment.objects.all()
     context = {
         'equipment_list': equipment_list,
     }
-    return render(request, 'equipment/equipment_list.html', context)
+    return render(request, 'equipment/equipment.html', context)
