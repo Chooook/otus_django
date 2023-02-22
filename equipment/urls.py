64c1a6729728a387ff2025d9221906e92c6d8bad
Egor Batarchuk <egr311@ya.ru>
2023-02-22 20:31:35 +0300

    add About url route

--- equipment/urls.py
+++ equipment/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import equipment_view
+from .views import equipment_view, about_view
 
 app_name = 'equipment'
 
 urlpatterns = [
     path('', equipment_view),
+    path('about', about_view),
 ]
