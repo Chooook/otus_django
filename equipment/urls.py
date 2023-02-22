c089b30242829f7965ccf332c1df231c1802fc1d
Egor Batarchuk <egr311@ya.ru>
2023-02-22 21:43:31 +0300

    add(urls.py) category_list link

--- equipment/urls.py
+++ equipment/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import equipment_view, about_view
+from . import views
 
 app_name = 'equipment'
 
 urlpatterns = [
-    path('', equipment_view, name='equipment'),
-    path('about', about_view, name='about'),
+    path('', views.equipment_view, name='equipment'),
+    path('about', views.about_view, name='about'),
+    path('category/list',
+         views.CategoryListView.as_view(),
+         name='category-list'),
 ]
