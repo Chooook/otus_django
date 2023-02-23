0fb0394c33c719b82eed60a313d4d3121410c6c6
Egor Batarchuk <egr311@ya.ru>
2023-02-23 20:45:10 +0300

    add(equipment models) Mixin abstract class with default timestamp fields

--- equipment/models.py
+++ equipment/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 
 
-class Category(models.Model):
+class TimestampMixin(models.Model):
+
+    class Meta:
+        abstract = True
+
+    create = models.DateTimeField(name='created_at', auto_now_add=True)
+    update = models.DateTimeField(name='updated_at', auto_now=True)
+
+
+class Category(TimestampMixin):
     name = models.CharField(unique=True, max_length=32)
 
     def __str__(self):
         return self.name
 
 
-class Equipment(models.Model):
+class Equipment(TimestampMixin):
 
     class Meta:
         unique_together = ('name', 'category')
