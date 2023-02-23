84b2a9cb215f3f62899dd84e8e1fc592c98b2799
Egor Batarchuk <egr311@ya.ru>
2023-02-23 20:46:28 +0300

    add(equipment admin) extended category model view in admin site

--- equipment/admin.py
+++ equipment/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Category, Equipment
 
 
-admin.site.register(Category)
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+
+
+admin.site.register(Category, CategoryModelAdmin)
 admin.site.register(Equipment)
