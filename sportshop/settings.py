69adcec7817568efc1e6a20de8e412de97afc63d
Egor Batarchuk <egr311@ya.ru>
2023-02-23 17:18:01 +0300

    add(settings) media paths

--- sportshop/settings.py
+++ sportshop/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [Path(BASE_DIR, 'static'), ]
 # STATIC_ROOT = Path('static') for production (manage.py collectstatic)
+MEDIA_ROOT = Path(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
