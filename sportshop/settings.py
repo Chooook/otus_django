6fcb394b7273603f9059002dc6fda259d4cb7522
Egor Batarchuk <egr311@ya.ru>
2023-02-23 17:10:47 +0300

    add(static) bootstrap 5 css and js connection

--- sportshop/settings.py
+++ sportshop/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [Path(BASE_DIR, 'static'), ]
+# STATIC_ROOT = Path('static') for production (manage.py collectstatic)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
