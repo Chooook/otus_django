148f189574ec2123a7636ccfb254da3b923d3eb0
Egor Batarchuk <egr311@ya.ru>
2023-02-24 17:47:30 +0300

    add(equipments commands) mixer way to generate data

---
+++ equipment/management/commands/mixer.py
@@ -0,0 +1,22 @@
+from django.core.management import BaseCommand
+from mixer.backend.django import mixer
+
+from equipment.models import Product, Equipment, Supplier, Category
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        Product.objects.all().delete()
+        Equipment.objects.all().delete()
+        Supplier.objects.all().delete()
+        Category.objects.all().delete()
+
+        for _ in range(5):
+            mixer.blend(Product)
+
+        for _ in range(5):
+            mixer.blend(Product, type__type='MyEqName')
+
+        for _ in range(5):
+            mixer.blend(Product, price=100)
