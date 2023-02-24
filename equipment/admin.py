a2f801a9f6acbb9664bc29877e6f2052bee631fb
Egor Batarchuk <egr311@ya.ru>
2023-02-24 15:22:44 +0300

    refactor(equipment admin) product admin view optimisation in field suppliers

--- equipment/admin.py
+++ equipment/admin.py
@@ -2,18 +2,19 @@ from django.contrib import admin
 from .models import Category, Equipment, Product, DebugEquipment, Supplier
 
 
+@admin.register(Product)
 class ProductModelAdmin(admin.ModelAdmin):
-    @staticmethod
-    def display_suppliers(obj):
-        suppliers = obj.supplier.all()
-        return ';'.join(sup.name for sup in suppliers)
-
     list_display = ('name', 'created_at', 'updated_at', 'display_suppliers')
+
+    def display_suppliers(self, obj):
+        suppliers = obj.suppliers.values_list('name', flat=True)
+        return ';'.join(suppliers)
+    display_suppliers.short_description = 'suppliers'
 
 
 admin.site.register(Category)
 admin.site.register(Equipment)
-admin.site.register(Product, ProductModelAdmin)
+# admin.site.register(Product, ProductModelAdmin)
 admin.site.register(DebugEquipment)
 # admin.site.register(Supplier)
 
