6359175726091cd6fd2a98a0fe99ad1d176e13c3
Egor Batarchuk <egr311@ya.ru>
2023-02-24 12:43:48 +0300

    add(equipment admin) equipment debug and products register

--- equipment/admin.py
+++ equipment/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Equipment
+from .models import Category, Equipment, Product, DebugEquipment
 
 
 class CategoryModelAdmin(admin.ModelAdmin):
@@ -8,3 +8,5 @@ class CategoryModelAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryModelAdmin)
 admin.site.register(Equipment)
+admin.site.register(Product)
+admin.site.register(DebugEquipment)
