dd5a7af69a6cc1523d368ea2d1d0f2ef7f0856df
Egor Batarchuk <egr311@ya.ru>
2023-02-24 14:48:03 +0300

    refactor(equipment admin) category admin view del, add product admin view

--- equipment/admin.py
+++ equipment/admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from .models import Category, Equipment, Product, DebugEquipment, Supplier
 
 
-class CategoryModelAdmin(admin.ModelAdmin):
+class ProductModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'created_at', 'updated_at')
 
 
-admin.site.register(Category, CategoryModelAdmin)
+admin.site.register(Category)
 admin.site.register(Equipment)
-admin.site.register(Product)
+admin.site.register(Product, ProductModelAdmin)
 admin.site.register(DebugEquipment)
 # admin.site.register(Supplier)
 
