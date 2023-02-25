a27709758aba9143c33da3a200adc8315030be56
Egor Batarchuk <egr311@ya.ru>
2023-02-25 18:55:16 +0300

    fix(equipment models) rename type to item_type

--- equipment/models.py
+++ equipment/models.py
@@ -43,13 +43,13 @@ class Category(models.Model):
 class Equipment(models.Model):
 
     class Meta:
-        unique_together = ('type', 'category')
-    # TODO change 'type' attribute to some another
-    type = models.CharField(max_length=64)
+        unique_together = ('item_type', 'category')
+
+    item_type = models.CharField(max_length=64)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.type
+        return self.item_type
 
     def buy(self):
         print('something')
@@ -57,7 +57,7 @@ class Equipment(models.Model):
 
 class Product(TimestampMixin):
     name = models.CharField(unique=True, max_length=64)
-    type = models.ForeignKey(Equipment, on_delete=models.CASCADE)
+    item_type = models.ForeignKey(Equipment, on_delete=models.CASCADE)
     suppliers = models.ManyToManyField(Supplier)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True)
