d048658cd6584c4097d1c4dd364a1548636404e9
Egor Batarchuk <egr311@ya.ru>
2023-02-27 17:21:31 +0300

    add(equipment urls) api default router and category routes register

--- equipment/urls.py
+++ equipment/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
+from rest_framework import routers
 
-from . import views
+from . import views, api_views
 
 app_name = 'equipment'
 
+router = routers.DefaultRouter()
+router.register('category', api_views.CategoryModelViewSet)
+
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('',
          views.IndexTemplateView.as_view(),
          name='index'),
