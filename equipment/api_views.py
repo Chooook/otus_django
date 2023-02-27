230bd2306af1542bd50d24095cbf3691903a91f7
Egor Batarchuk <egr311@ya.ru>
2023-02-27 20:04:41 +0300

    add(equipment api views) renderers usage

--- equipment/api_views.py
+++ equipment/api_views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework import renderers
+# from djangorestframework_camel_case import render
 
 from .models import Category
 from .serializers import CategorySerializer
@@ -8,6 +9,9 @@ from .serializers import CategorySerializer
 class CategoryModelViewSet(viewsets.ModelViewSet):
     # how multiple renderers works???
     renderer_classes = [renderers.JSONRenderer, renderers.AdminRenderer]
+    # for JavaScript frontend developers comfort
+    # renderer_classes = [render.CamelCaseJSONRenderer,
+    #                     render.CamelCaseBrowsableAPIRenderer]
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
