0b9d5b70e238d631396b596eb648408143281f60
Egor Batarchuk <egr311@ya.ru>
2023-02-27 19:58:56 +0300

    add(equipment api views) renderers usage

--- equipment/api_views.py
+++ equipment/api_views.py
@@ -1,10 +1,13 @@
 from rest_framework import viewsets
+from rest_framework import renderers
 
 from .models import Category
 from .serializers import CategorySerializer
 
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
+    # how multiple renderers works???
+    renderer_classes = [renderers.JSONRenderer, renderers.AdminRenderer]
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
